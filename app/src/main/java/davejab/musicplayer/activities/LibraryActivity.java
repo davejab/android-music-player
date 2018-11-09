e959a6788f68b392ff598582a0b7f9bebf7d808c
davejab <daveabrante@live.co.uk>
2018-11-09 00:48:38 +0000

    Adding basic sliding panel view for player controls

--- app/src/main/java/davejab/musicplayer/activities/LibraryActivity.java
+++ app/src/main/java/davejab/musicplayer/activities/LibraryActivity.java
@@ -3,7 +3,11 @@ package davejab.musicplayer.activities;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+
+import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
 import davejab.musicplayer.R;
 import davejab.musicplayer.activities.fragments.ArtistFragment;
@@ -15,6 +19,7 @@ import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 public class LibraryActivity extends FragmentActivity {
 
     private TextView appBarText;
+    private SlidingUpPanelLayout mLayout;
 
     private static String[] PERMISSIONS = {
             READ_EXTERNAL_STORAGE,
@@ -24,13 +29,32 @@ public class LibraryActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_library);
+        setContentView(R.layout.activity_main);
 
         // TODO Move this elsewhere
         // Get required permissions
         requestPermissions(PERMISSIONS, 0);
 
         this.appBarText = findViewById(R.id.app_bar_title);
+
+        mLayout = (SlidingUpPanelLayout) findViewById(R.id.sliding_layout);
+        mLayout.addPanelSlideListener(new SlidingUpPanelLayout.PanelSlideListener() {
+            @Override
+            public void onPanelSlide(View panel, float slideOffset) {
+                Log.i("DABRA", "onPanelSlide, offset " + slideOffset);
+            }
+
+            @Override
+            public void onPanelStateChanged(View panel, SlidingUpPanelLayout.PanelState previousState, SlidingUpPanelLayout.PanelState newState) {
+                Log.i("DABRA", "onPanelStateChanged " + newState);
+            }
+        });
+        mLayout.setFadeOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mLayout.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
+            }
+        });
 
         switchFragment(ArtistFragment.getArtistFragment(new Artist(getContentResolver()).toList()));
     }
@@ -45,5 +69,4 @@ public class LibraryActivity extends FragmentActivity {
     public void setAppBarText(String text){
         this.appBarText.setText(text);
     }
-
 }
